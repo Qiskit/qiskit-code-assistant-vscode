7f83e811f2111266fb816612419dd86fef5a3a72
Yeray Darias <Yeray.Darias@ibm.com>
2018-03-09 17:20:16 +0100

    Improved parser tests with TS syntax

--- server/test/parser.test.ts
+++ server/test/parser.test.ts
@@ -1,16 +1,40 @@
 import * as assert from 'assert';
 import { parse } from '../src/antlr/parser'
+import { ParserResult } from '../src/tools/parserModel';
 
-describe('A parse function', function() {
+let doesNotContainErrors = (result: ParserResult) => {
+  return result.errors.length === 0;
+}
 
-  describe('given an empty input', function() {
+describe('A parse function', () => {
 
-    it('will end without errors', function() {
-        let result = parse('');
+  describe('with an empty input', () => {
 
-        assert.ok(result.errors.length === 0);
+    it('will end without errors', () => {
+      let result = parse('');
+      doesNotContainErrors(result);
     });
 
   });
-  
+
+  describe('with headers', () => {
+    it('defining only ASM version will end without errors', () => {
+      let input = 'OPENQASM 2.0;';
+
+      let result = parse(input);
+      doesNotContainErrors(result);
+    });
+
+    it('including a functions library will end without errors', () => {
+      let input =
+        `
+        OPENQASM 2.0;
+        include QELIB.INC;
+        `;
+
+      let result = parse(input);
+      doesNotContainErrors(result);
+    });
+  })
+
 });
