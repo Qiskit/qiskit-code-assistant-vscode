0d38f098c5c2cb2ec0c7fe3d7d0070bf443e0440
Yeray Darias <Yeray.Darias@ibm.com>
2018-03-15 11:38:49 +0100

    Improved server test suite behavior

---
+++ server/src/qasm/model.js
@@ -0,0 +1,7 @@
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+var ParseErrorLevel;
+(function (ParseErrorLevel) {
+    ParseErrorLevel[ParseErrorLevel["ERROR"] = 0] = "ERROR";
+    ParseErrorLevel[ParseErrorLevel["WARNING"] = 1] = "WARNING";
+})(ParseErrorLevel = exports.ParseErrorLevel || (exports.ParseErrorLevel = {}));
---
+++ server/test/compilation.test.ts
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+
+describe('A compilation tool', () => {
+
+    describe('foo', () => {
+        it('bar', () => {
+            expect(1).to.be.eq(-2);
+        });
+    });
+    
+});
