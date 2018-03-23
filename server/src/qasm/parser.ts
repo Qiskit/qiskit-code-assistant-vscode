099246a22ec95a1b4e2ad0b5edf3eec892457c0e
Yeray Darias <yeray.darias@ibm.com>
2018-03-23 10:19:13 +0100

    Extracted tree printer as a class on its own

--- server/src/qasm/parser.ts
+++ server/src/qasm/parser.ts
@@ -1,3 +1,13 @@
+/*
+  Copyright IBM Corp. 2018. All Rights Reserved.
+
+  This code may only be used under the Apache 2.0 license found at
+  http://www.apache.org/licenses/LICENSE-2.0.txt.
+
+  Authors:
+  - Yeray Darias <yeray.darias@ibm.com>
+*/
+
 'use strict';
 
 import { ANTLRInputStream, CommonTokenStream, ANTLRErrorListener, CommonToken, Token, Recognizer, RecognitionException, ConsoleErrorListener, ParserRuleContext } from 'antlr4ts';
@@ -6,6 +16,7 @@ import { ParserResult, ParserError, ParseErrorLevel } from '../qasm/model';
 import { QasmLexer } from './antlr/QasmLexer';
 import { QasmParser, CodeContext } from './antlr/QasmParser';
 import { Override } from 'antlr4ts/Decorators';
+import { TreePrinter } from './compiler/treePrinter';
 
 export class Parser {
 
@@ -15,38 +26,12 @@ export class Parser {
     
         let tree = parser.code();
 
-        
-        this.printTree(parser.ruleNames, tree);
+        // TreePrinter.print(parser.ruleNames, tree);
     
         return {
             ast: tree,
             errors: errorListener.errors
         };
-    }
-
-    private printTree(ruleNames: string[], root: ParserRuleContext): void {
-        console.log(`START : ${ruleNames[root.ruleIndex]} > `);
-        this.printChildren(ruleNames, root.children as ParserRuleContext[], 1);
-    }
-
-    private printChildren(ruleNames: string[], children: ParserRuleContext[], level: number): void {
-        children.forEach((child) => { 
-            let spaces = '';
-            let spacePosition = 0;
-            while (spacePosition < level) {
-                spaces += '|  ';
-                spacePosition++;
-            }
-            if (ruleNames[child.ruleIndex] == undefined) {
-                console.log(`${spaces}${child.text}`);
-            } else {
-                console.log(`${spaces}${ruleNames[child.ruleIndex]} > `);
-            }
-            if (child.childCount > 0) {
-                this.printChildren(ruleNames, child.children as ParserRuleContext[], ++level);
-                level--;
-            }
-        });
     }
 
     private buildQasmParser(input: string, errorListener: ErrorListener): QasmParser {
