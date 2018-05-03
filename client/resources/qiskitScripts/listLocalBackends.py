59abed2a984ad87e3ccd3591efdae74408e33721
Juan-Cruz <juan.cruz@ibm.com>
2018-05-03 17:27:34 +0200

    Minor fix in discover local backends script

--- client/resources/qiskitScripts/listLocalBackends.py
+++ client/resources/qiskitScripts/listLocalBackends.py
@@ -1,4 +1,8 @@
 from qiskit import backends
 import json
 
-print(json.dumps(backends.discover_local_backends(), indent=2, sort_keys=True))
+def main():
+    print(json.dumps(backends.discover_local_backends(), indent=2, sort_keys=True))    
+
+if __name__ == '__main__':
+    main()
