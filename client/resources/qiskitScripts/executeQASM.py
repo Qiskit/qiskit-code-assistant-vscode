f88cfbb7f3297c5580b7a692def766b9c64dd026
Abdón Rodríguez Davila <a@abdonrd.com>
2018-06-08 15:31:00 +0200

    Consistent license across all the project

--- client/resources/qiskitScripts/executeQASM.py
+++ client/resources/qiskitScripts/executeQASM.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2018, IBM.
+#
+# This source code is licensed under the Apache License, Version 2.0 found in
+# the LICENSE.txt file in the root directory of this source tree.
+
 from qiskit.wrapper import load_qasm_file
 from qiskit import execute
 import argparse
@@ -7,14 +12,14 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--file')
-    
+
     args = vars(parser.parse_args())
 
     qc = load_qasm_file(args['file'])
 
     job_sim = execute(qc, "local_qasm_simulator")
     result = job_sim.result()
-    
+
     print(json.dumps(result._result, indent=2, sort_keys=True))
 
 if __name__ == '__main__':
--- client/resources/qiskitScripts/listLocalBackends.py
+++ client/resources/qiskitScripts/listLocalBackends.py
@@ -1,8 +1,13 @@
+# Copyright (c) 2018, IBM.
+#
+# This source code is licensed under the Apache License, Version 2.0 found in
+# the LICENSE.txt file in the root directory of this source tree.
+
 from qiskit import available_backends
 import json
 
 def main():
-    print(json.dumps(available_backends({'local': True}), indent=2, sort_keys=True))    
+    print(json.dumps(available_backends({'local': True}), indent=2, sort_keys=True))
 
 if __name__ == '__main__':
     main()
