84da325d295330ca03c641c865953056bc55412d
Juan-Cruz <juan.cruz@ibm.com>
2018-06-18 12:56:05 +0200

    Adding IBM Q 20 Tokyo to the discover remote backends available command

--- client/resources/qiskitScripts/listRemoteBackends.py
+++ client/resources/qiskitScripts/listRemoteBackends.py
@@ -8,11 +8,13 @@ from IBMQuantumExperience import IBMQuantumExperience
 import argparse
 import json
 
+
 def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--apiToken')
-    parser.add_argument('--url', nargs='?', default='https://quantumexperience.ng.bluemix.net/api')
+    parser.add_argument('--url', nargs='?',
+                        default='https://quantumexperience.ng.bluemix.net/api')
     parser.add_argument('--hub', nargs='?', default=None)
     parser.add_argument('--group', nargs='?', default=None)
     parser.add_argument('--project', nargs='?', default=None)
@@ -26,11 +28,13 @@ def main():
     if (args['hub'] is None) or (args['group'] is None) or (args['project'] is None):
         register(args['apiToken'], args['url'])
     else:
-        register(args['apiToken'], args['url'], args['hub'], args['group'], args['project'])
+        register(args['apiToken'], args['url'], args['hub'],
+                 args['group'], args['project'])
 
     backs = available_backends({'local': False})
 
     publicNameDevices = {}
+    publicNameDevices['ibmq_20_tokyo'] = "IBM Q 20 Tokyo"
     publicNameDevices['QS1_1'] = "IBM Q 20 Austin"
     publicNameDevices['ibmqx5'] = "IBM Q 16 Rueschlikon"
     publicNameDevices['ibmqx4'] = "IBM Q 5 Tenerife"
@@ -41,12 +45,13 @@ def main():
         statusDevices = []
         for back in backs:
             deviceStatus = {}
-            deviceStatus['name']= publicNameDevices[get_backend(back).name]
-            deviceStatus['status']= get_backend(back).status
+            deviceStatus['name'] = publicNameDevices[get_backend(back).name]
+            deviceStatus['status'] = get_backend(back).status
             statusDevices.append(deviceStatus)
         print(json.dumps(statusDevices, indent=2, sort_keys=True))
     else:
         print(json.dumps(backs, indent=2, sort_keys=True))
+
 
 if __name__ == '__main__':
     main()
