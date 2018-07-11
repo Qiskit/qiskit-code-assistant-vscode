f05aa8f59023d2d1cf1a7ccb04bd642868e3a57c
Yeray Darias <yeray.darias@ibm.com>
2018-07-11 11:37:17 +0200

    Allowed multi-scope at the persistent ST

--- server/src/qiskit/compiler/persistentSymbolTable.ts
+++ server/src/qiskit/compiler/persistentSymbolTable.ts
@@ -9,42 +9,143 @@
 
 'use strict';
 
-import { BuiltInTypeSymbol, Symbol, Type } from '../../tools/symbolTable';
+import { BuiltInTypeSymbol, Symbol } from '../../tools/symbolTable';
 import { QiskitSymbols } from './qiskitSymbolTable';
+import { start } from 'repl';
+
+const MAX_LINE = 35000;
 
 export class PersistentSymbolTable {
-    constructor(private rootScope: Scope) {}
+    private currentScope: Scope;
+
+    constructor(private rootScope: Scope) {
+        this.currentScope = rootScope;
+    }
 
     lookup(symbolName: string, line?: number): Symbol {
         return this.rootScope.lookup(symbolName, line);
     }
 
     define(symbol: Symbol, line: number): void {
-        this.rootScope.define(symbol, line);
+        this.currentScope.define(symbol, line);
+    }
+
+    push(scopeName: string, line: number): void {
+        let localScope = new Scope(this.currentScope, scopeName, line);
+        this.currentScope.childs.push(localScope);
+
+        this.currentScope = localScope;
+    }
+
+    pop(line: number): void {
+        this.currentScope.closeScopeAt(line);
+        this.currentScope = this.currentScope.parentScope;
+    }
+
+    print() {
+        console.log('Printing symbol table >>>>>>>>');
+        this.rootScope.print();
+        console.log('<<<<<<<<<<<<<<<<');
     }
 }
 
 class Scope {
     dictionary: Map<string, VariableDefinition[]> = new Map();
-    childs: Scope[];
+    childs: Scope[] = [];
 
-    lookup(symbolName: string, line?: number): Symbol {
-        return this.dictionary.get(symbolName)[0].symbol;
+    public endLine = MAX_LINE;
+
+    constructor(public parentScope: Scope, public name: string, public startLine = 1) {}
+
+    lookup(symbolName: string, line = MAX_LINE): Symbol {
+        if (!this.dictionary.has(symbolName)) {
+            let inRangeScope = this.childs.find(scope => scope.inScopeAt(line));
+            if (inRangeScope) {
+                return inRangeScope.lookup(symbolName, line);
+            }
+
+            return null;
+        }
+
+        let result = this.dictionary.get(symbolName).find(variableDefinition => {
+            return variableDefinition.nameEquals(symbolName) && variableDefinition.inScope(line);
+        });
+        if (result) {
+            return result.symbol;
+        }
+
+        return null;
     }
 
-    define(symbol: Symbol, startLine = 1, endLine = Number.MAX_SAFE_INTEGER) {
-        let variableDefinition = new VariableDefinition(symbol, startLine, endLine);
-        this.dictionary.set(symbol.name, [variableDefinition]);
+    define(symbol: Symbol, startLine = 1, endLine = MAX_LINE) {
+        if (this.dictionary.has(symbol.name)) {
+            let lastDefinition = this.dictionary.get(symbol.name).pop();
+            lastDefinition.endLine = startLine - 1;
+
+            this.dictionary.get(symbol.name).push(lastDefinition);
+
+            let variableDefinition = new VariableDefinition(symbol, startLine, endLine);
+            this.dictionary.get(symbol.name).push(variableDefinition);
+        } else {
+            let variableDefinition = new VariableDefinition(symbol, startLine, endLine);
+            this.dictionary.set(symbol.name, [variableDefinition]);
+        }
+    }
+
+    closeScopeAt(line: number): void {
+        this.dictionary.forEach(variables => variables.forEach(variable => (variable.endLine = line)));
+        this.endLine = line;
+    }
+
+    inScopeAt(line: number): boolean {
+        return line >= this.startLine && line <= this.endLine;
+    }
+
+    print(level = 1): void {
+        let tabs = `${this.tabsGenerator(level - 1)}`;
+        console.log(`${tabs}Scope "${this.name}" from ${this.startLine} to ${this.endLine} => `);
+
+        this.printEntries(level);
+
+        this.childs.forEach(child => child.print(level + 1));
+    }
+
+    private printEntries(level: number) {
+        this.dictionary.forEach(entry => {
+            console.log(`${this.tabsGenerator(level)}${entry.toString()}`);
+        });
+    }
+
+    private tabsGenerator(amount: number): string {
+        let tabs = '';
+
+        for (let i = 0; i < amount; i++) {
+            tabs += '\t';
+        }
+
+        return tabs;
     }
 }
 
 class VariableDefinition {
     constructor(public symbol: Symbol, public startLine: number, public endLine: number) {}
+
+    nameEquals(name: string): boolean {
+        return this.symbol.name === name;
+    }
+
+    inScope(line: number): boolean {
+        return line >= this.startLine && line <= this.endLine;
+    }
+
+    toString() {
+        return `{ ${this.symbol} from: ${this.startLine} to: ${this.endLine}] }`;
+    }
 }
 
 export namespace QiskitInitialScope {
     export function create(): Scope {
-        let scope = new Scope();
+        let scope = new Scope(null, 'global');
 
         scope.define(new BuiltInTypeSymbol(QiskitSymbols.void));
         scope.define(new BuiltInTypeSymbol(QiskitSymbols.object));
--- server/test/persistentSymbolTable.test.ts
+++ server/test/persistentSymbolTable.test.ts
@@ -14,19 +14,69 @@ import { PersistentSymbolTable, QiskitInitialScope } from '../src/qiskit/compile
 import { VariableSymbol, QiskitSymbols } from '../src/qiskit/compiler/qiskitSymbolTable';
 
 let secondLine = 2;
+let thirdLine = 3;
+let fithLine = 5;
+let sixthLine = 6;
+let seventhLine = 7;
 
 describe('A persistent symbol table', () => {
-    let initialScope = QiskitInitialScope.create();
-    let symbolTable = new PersistentSymbolTable(initialScope);
-
     describe('with a variable b in a global scope', () => {
-        let numberB = new VariableSymbol('b', symbolTable.lookup(QiskitSymbols.number));
-        symbolTable.define(numberB, secondLine);
+        let symbolTable: PersistentSymbolTable;
 
-        it('is able to recover the original declaration of b', () => {
+        beforeEach(() => {
+            let initialScope = QiskitInitialScope.create();
+            symbolTable = new PersistentSymbolTable(initialScope);
+            let numberB = new VariableSymbol('b', symbolTable.lookup(QiskitSymbols.number));
+            symbolTable.define(numberB, secondLine);
+        });
+
+        it('is able to recover the original declaration of b as a number', () => {
             let result = symbolTable.lookup('b');
 
-            expect(result.type).to.be.eq(numberB.type);
+            expect(result.type).to.be.eq(symbolTable.lookup(QiskitSymbols.number));
+        });
+
+        it('is able to recover the last redefinition of b as a string', () => {
+            let stringB = new VariableSymbol('b', symbolTable.lookup(QiskitSymbols.string));
+            symbolTable.define(stringB, thirdLine);
+
+            let result = symbolTable.lookup('b');
+
+            expect(result.type).to.be.eq(stringB.type);
+        });
+
+        it('is able to recover a previous definition of b as a number', () => {
+            let stringB = new VariableSymbol('b', symbolTable.lookup(QiskitSymbols.string));
+            symbolTable.define(stringB, thirdLine);
+
+            let result = symbolTable.lookup('b', secondLine);
+
+            expect(result.type).to.be.eq(symbolTable.lookup(QiskitSymbols.number));
+        });
+    });
+
+    describe('with a variable c in a local scope', () => {
+        let initialScope = QiskitInitialScope.create();
+        let symbolTable = new PersistentSymbolTable(initialScope);
+        let numberB = new VariableSymbol('b', symbolTable.lookup(QiskitSymbols.number));
+        symbolTable.define(numberB, secondLine);
+        symbolTable.push('new scope', thirdLine);
+        let stringC = new VariableSymbol('c', symbolTable.lookup(QiskitSymbols.string));
+        symbolTable.define(stringC, fithLine);
+        symbolTable.pop(seventhLine);
+
+        it('is not able to recover the definition of c at the global scope', () => {
+            let result = symbolTable.lookup('c');
+
+            expect(result).to.be.null;
+        });
+
+        it('is able to recover the definition of c event with the scope closed', () => {
+            symbolTable.print();
+
+            let result = symbolTable.lookup('c', sixthLine);
+
+            expect(result.type).to.be.eq(stringC.type);
         });
     });
 });
