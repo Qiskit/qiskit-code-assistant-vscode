4a04d4660101cde244b0f41716715f9849a8f43e
Juan-Cruz <juan.cruz@ibm.com>
2018-07-25 14:31:09 +0200

    Adding tests for pypiExecutor

---
+++ client/test/pip/pypiExecutor.test.ts
@@ -0,0 +1,26 @@
+/**
+ * @license
+ *
+ * Copyright (c) 2018, IBM.
+ *
+ * This source code is licensed under the Apache License, Version 2.0 found in
+ * the LICENSE.txt file in the root directory of this source tree.
+ */
+import { PyPiExecutor } from '../../src/pip/pypiExecutor';
+
+describe('A PipyExecutor', () => {
+    describe('testing pypi commands using qiskit package', () => {
+        let pypiExecutor = new PyPiExecutor();
+
+        it('should get info about the package', async () => {
+            // tslint:disable-next-line
+            expect.assertions(4);
+            const result = await pypiExecutor.getPackageInfo('qiskit');
+            expect(result.location).toBe('https://pypi.org/project/qiskit/');
+            expect(result.name).toBe('qiskit');
+            expect(result.summary).toBeDefined();
+            console.log(result.version.toString());
+            expect(result.version.toString()).toMatch(/.*\..*\..*/g);
+        });
+    });
+});
