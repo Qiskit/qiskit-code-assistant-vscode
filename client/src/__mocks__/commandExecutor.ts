14011aa4a846ccc5154a52c226d15865759d3cae
Juan-Cruz <juan.cruz@ibm.com>
2018-07-26 11:13:34 +0200

    Removing the integration from the test to be only unitary. Fixing issues related to linting

--- client/src/__mocks__/commandExecutor.ts
+++ client/src/__mocks__/commandExecutor.ts
@@ -8,7 +8,6 @@
  */
 
 import * as Q from 'q';
-import * as nodeChildProcess from 'child_process';
 
 interface IExecOptions {
     cwd?: string;
@@ -22,26 +21,8 @@ interface IExecOptions {
 
 export namespace CommandExecutor {
     export function exec(command: string, args: string[] = [], options: IExecOptions = {}): Q.Promise<string> {
-        let outcome = Q.defer<string>();
+        console.log(command, args, options);
 
-        nodeChildProcess.exec(
-            command + ' ' + args.join(' '),
-            options,
-            (error: Error, stdout: string, stderr: string) => {
-                if (error) {
-                    // Dirty trick, read below.
-                    let errorString = stdout + stderr;
-                    outcome.reject(errorString);
-                } else {
-                    // Dirty trick, some commands outputs successfully commands,
-                    // like --version to stderr (WTF!). Python interpreter is an
-                    // example, unfortunately.
-                    let outputString = stdout + stderr;
-                    outcome.resolve(outputString);
-                }
-            }
-        );
-
-        return outcome.promise;
+        return Q(`Python 3.6.3 :: Anaconda custom (64-bit)`);
     }
 }
