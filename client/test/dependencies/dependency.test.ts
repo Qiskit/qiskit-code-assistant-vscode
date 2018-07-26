ae06d74bda79661a393fb80176fd4b8b6fea9efd
Juan-Cruz <juan.cruz@ibm.com>
2018-07-26 16:18:48 +0200

    Improve readability

--- client/test/dependencies/dependency.test.ts
+++ client/test/dependencies/dependency.test.ts
@@ -11,31 +11,29 @@ jest.mock('../../src/commandExecutor');
 import { Version } from '../../src/version';
 
 describe('Dependency', () => {
-    describe('-related testing things', () => {
-        it('is installed?', () => {
-            // tslint:disable-next-line
-            //expect.assertions(1);
-            const depName = 'python';
-            // tslint:disable-next-line
-            const depVersion = new Version(3, 5, 0);
+    it('is installed?', () => {
+        // tslint:disable-next-line
+        //expect.assertions(1);
+        const depName = 'python';
+        // tslint:disable-next-line
+        const depVersion = new Version(3, 5, 0);
 
-            const dep = new Dependency(depName, depVersion);
-            return dep.isInstalled().then(result => {
-                expect(JSON.stringify(result)).toMatch(/.*name.*python.*/g);
-            });
+        const dep = new Dependency(depName, depVersion);
+        return dep.isInstalled().then(result => {
+            expect(JSON.stringify(result)).toMatch(/.*name.*python.*/g);
         });
+    });
 
-        it('the required version is not installed', () => {
-            // tslint:disable-next-line
-            expect.assertions(1);
-            const depName = 'python';
-            // tslint:disable-next-line
-            const depVersion = new Version(3, 6, 6);
+    it('the required version is not installed', () => {
+        // tslint:disable-next-line
+        expect.assertions(1);
+        const depName = 'python';
+        // tslint:disable-next-line
+        const depVersion = new Version(3, 6, 6);
 
-            const dep = new Dependency(depName, depVersion);
-            return dep.isInstalled().catch(result => {
-                expect(result).toBe(`Version >= 3.6.6 of package python is required`);
-            });
+        const dep = new Dependency(depName, depVersion);
+        return dep.isInstalled().catch(result => {
+            expect(result).toBe(`Version >= 3.6.6 of package python is required`);
         });
     });
 });
