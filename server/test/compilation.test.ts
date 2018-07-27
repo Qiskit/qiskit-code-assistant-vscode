83c5b34baa5592e42b5598b5dd5b373ef080cfe9
Yeray Darias <ydarias@gmail.com>
2018-07-27 12:42:38 +0200

    Completed Compilation tool Jest tests

--- server/test/compilation.test.ts
+++ server/test/compilation.test.ts
@@ -9,35 +9,38 @@
 
 'use strict';
 
-import { expect } from 'chai';
 import { mock, instance } from 'ts-mockito';
-import { createConnection, IConnection, IPCMessageWriter, IPCMessageReader, CompletionItem } from 'vscode-languageserver';
+import {
+    createConnection,
+    IConnection,
+    IPCMessageWriter,
+    IPCMessageReader,
+    CompletionItem
+} from 'vscode-languageserver';
 import { CompilationTool } from '../src/compilation';
 import { QASMParser } from '../src/qasm/parser';
 import { QASMSuggester } from '../src/qasm/suggester';
 
 describe('A compilation tool', () => {
+    let messageReaderMock: IPCMessageReader = mock(IPCMessageReader);
+    let messageWriterMock: IPCMessageWriter = mock(IPCMessageWriter);
 
-    let messageReaderMock:IPCMessageReader = mock(IPCMessageReader);
-    let messageWriterMock:IPCMessageWriter = mock(IPCMessageWriter);
+    let messageReader: IPCMessageReader = instance(messageReaderMock);
+    let messageWriter: IPCMessageWriter = instance(messageWriterMock);
 
-    let messageReader:IPCMessageReader = instance(messageReaderMock);
-    let messageWriter:IPCMessageWriter = instance(messageWriterMock);
+    let connection: IConnection = createConnection(messageReader, messageWriter);
 
-    let connection:IConnection = createConnection(messageReader, messageWriter);
+    let compilationTool: CompilationTool = new CompilationTool(connection, new QASMParser(), new QASMSuggester());
 
-    let compilationTool:CompilationTool = new CompilationTool(connection, new QASMParser(), new QASMSuggester());
-    
     describe('when asked for a completion item detail', () => {
         it('will not fail if there is no option available', () => {
-            let item:CompletionItem = {
+            let item: CompletionItem = {
                 label: 'foo'
             };
 
             let result = compilationTool.completionDetailsFor(item);
 
-            expect(result.detail).to.be.eq('');
+            expect(result.detail).toEqual('');
         });
     });
-
 });
