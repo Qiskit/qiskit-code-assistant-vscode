329c3422227f2692f3d8ba78c0980dd8f1e340e5
Juan-Cruz <juan.cruz@ibm.com>
2018-08-20 12:37:40 +0200

    Renaming aqua descriptors in test files

--- client/test/pip/packageInfoParser.test.ts
+++ client/test/pip/packageInfoParser.test.ts
@@ -21,7 +21,7 @@ describe('A PackageInfoParser', () => {
         License: Apache 2.0
         Location: /Users/qiskitter/Documents/workspace/qiskit-vscode/pyenv/lib/python3.6/site-packages
         Requires: IBMQuantumExperience, sympy, networkx, numpy, matplotlib, pillow, scipy, ply
-        Required-by: qiskit-acqua, qiskit-acqua-chemistry
+        Required-by: qiskit-aqua, qiskit-aqua-chemistry
         `;
 
         it('should detect the package name', () => {
