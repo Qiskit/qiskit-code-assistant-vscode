a8e5291686d933312232c92c21725388faf5ccb8
Juan Cruz-Benito <juan.cruz@ibm.com>
2018-10-26 12:47:38 +0200

    Adding support to execute QASM in Qiskit > v0.6

--- client/resources/qiskitScripts/executeQASM.py
+++ client/resources/qiskitScripts/executeQASM.py
@@ -3,26 +3,35 @@
 # This source code is licensed under the Apache License, Version 2.0 found in
 # the LICENSE.txt file in the root directory of this source tree.
 
-from qiskit.wrapper import load_qasm_file
-from qiskit import execute
+from qiskit import __version__
+from packaging import version
 import argparse
 import json
+import subprocess
+import os
 
+if (version.parse(__version__) > version.parse("0.5") and 
+    version.parse(__version__) < version.parse("0.6")):
+    import executeQASMv05
+
+if (version.parse(__version__) > version.parse("0.6")):
+    import executeQASMv06
 
 def main():
     parser = argparse.ArgumentParser()
-
     parser.add_argument('--file')
-
     args = vars(parser.parse_args())
 
-    qc = load_qasm_file(args['file'])
+    if (version.parse(__version__) > version.parse("0.5") and 
+        version.parse(__version__) < version.parse("0.6")):
 
-    job_sim = execute(qc, "local_qasm_simulator")
-    result = job_sim.result()
+        result = executeQASMv05.process(args['file'])
+        print(json.dumps(result._result, indent=2, sort_keys=True))
 
-    print(json.dumps(result._result, indent=2, sort_keys=True))
+    if (version.parse(__version__) > version.parse("0.6")):
 
+        result = executeQASMv06.process(args['file'])
+        print(json.dumps(result, indent=2, sort_keys=True))
 
 if __name__ == '__main__':
     main()
