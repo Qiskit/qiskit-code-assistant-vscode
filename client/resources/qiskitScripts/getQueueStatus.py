15e4f6665f4d6ffeb17449c90129d264675a8a69
Juan Cruz-Benito <juan.cruz@ibm.com>
2018-10-29 14:48:01 +0100

    Adding support to get the status of the queue in qiskit-terra >v0.6

--- client/resources/qiskitScripts/getQueueStatus.py
+++ client/resources/qiskitScripts/getQueueStatus.py
@@ -3,15 +3,23 @@
 # This source code is licensed under the Apache License, Version 2.0 found in
 # the LICENSE.txt file in the root directory of this source tree.
 
-from qiskit import available_backends, register
-from IBMQuantumExperience import IBMQuantumExperience
-import argparse
-import json
 import warnings
+import json
+import argparse
+from packaging import version
+from qiskit import __version__
+if (version.parse(__version__) > version.parse("0.5") and
+        version.parse(__version__) < version.parse("0.6")):
+    from qiskit import available_backends, register
+    from IBMQuantumExperience import IBMQuantumExperience
+
+if (version.parse(__version__) > version.parse("0.6")):
+    from qiskit import IBMQ
 
 
 def main():
     warnings.simplefilter('ignore')
+
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--apiToken')
@@ -26,28 +34,59 @@ def main():
     if (args['url'] is None):
         args['url'] = 'https://quantumexperience.ng.bluemix.net/api'
 
-    if ((args['hub'] is None)
-        or (args['group'] is None)
-            or (args['project'] is None)):
-        api = IBMQuantumExperience(args['apiToken'], {'url': args['url']})
-        register(args['apiToken'], args['url'])
-    else:
-        api = IBMQuantumExperience(args['apiToken'],
-                                   {'url': args['url'],
-                                    'hub': args['hub'],
-                                    'group': args['group'],
-                                    'project': args['project']})
-        register(args['apiToken'], args['url'], args['hub'],
-                 args['group'], args['project'])
+    if (version.parse(__version__) > version.parse("0.5") and
+            version.parse(__version__) < version.parse("0.6")):
 
-    backs = available_backends()
+        if (args['hub'] is None or args['group'] is None
+                or args['project'] is None):
+            api = IBMQuantumExperience(args['apiToken'], {'url': args['url']})
+            register(args['apiToken'], args['url'])
+        else:
+            api = IBMQuantumExperience(args['apiToken'],
+                                       {'url': args['url'],
+                                        'hub': args['hub'],
+                                        'group': args['group'],
+                                        'project': args['project']})
+            register(args['apiToken'], args['url'], args['hub'],
+                     args['group'], args['project'])
+
+        backs = available_backends({'local': False})
+
+    if (version.parse(__version__) > version.parse("0.6")):
+
+        if (args['hub'] is None or args['group'] is None
+                or args['project'] is None):
+            IBMQ.enable_account(args['apiToken'], args['url'])
+        else:
+            IBMQ.enable_account(args['apiToken'], url=args['url'],
+                                hub=args['hub'], group=args['group'],
+                                project=args['project'])
+
+        backs = [backend.name() for backend in IBMQ.backends()]
 
     for back in backs:
-        try:
-            back_status = api.backend_status(back)
-            print(json.dumps(back_status, indent=2, sort_keys=True))
-        except:
-            pass
+
+        if (version.parse(__version__) > version.parse("0.5") and
+                version.parse(__version__) < version.parse("0.6")):
+            print(json.dumps(api.backend_status(back), indent=2, sort_keys=True))
+        if (version.parse(__version__) > version.parse("0.6")):
+            print(json.dumps(parseBackendStatus(IBMQ.get_backend(
+                back).status()), indent=2, sort_keys=True))
+
+
+def parseBackendStatus(backendStatus):
+    return {
+        'name': backendStatus['name'],
+        'pending_jobs': backendStatus['pending_jobs'],
+        'available': parseAvailability(backendStatus)
+    }
+
+
+def parseAvailability(backendStatus):
+    try:
+        return backendStatus['available']
+    except KeyError:
+        return backendStatus['operational']
 
 
 if __name__ == '__main__':
