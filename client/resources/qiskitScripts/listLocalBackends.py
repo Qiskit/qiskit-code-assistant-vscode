d586510ee3b587a62386fd4078a878a73ef292d3
Juan Cruz-Benito <juan.cruz@ibm.com>
2018-10-29 13:11:12 +0100

    Adding support to discover local backeds in Qiskit-terra v0.6

--- client/resources/qiskitScripts/listLocalBackends.py
+++ client/resources/qiskitScripts/listLocalBackends.py
@@ -2,16 +2,29 @@
 #
 # This source code is licensed under the Apache License, Version 2.0 found in
 # the LICENSE.txt file in the root directory of this source tree.
-
-from qiskit import available_backends
-import json
 import warnings
+import json
+from qiskit import __version__
+from packaging import version
+
+if (version.parse(__version__) > version.parse("0.5") and
+        version.parse(__version__) < version.parse("0.6")):
+    from qiskit import available_backends
+
+if (version.parse(__version__) > version.parse("0.6")):
+    from qiskit import Aer
 
 
 def main():
     warnings.simplefilter('ignore')
-    print(json.dumps(available_backends(
-        {'local': True}), indent=2, sort_keys=True))
+    if (version.parse(__version__) > version.parse("0.5")
+            and version.parse(__version__) < version.parse("0.6")):
+        print(json.dumps(available_backends(
+            {'local': True}), indent=2, sort_keys=True))
+    if (version.parse(__version__) > version.parse("0.6")):
+        print(json.dumps([backend.name()
+                          for backend in Aer.backends()],
+                         indent=2, sort_keys=True))
 
 
 if __name__ == '__main__':
