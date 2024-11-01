b994f41cc79950873c8d896e1b94c606787e98c8
//va <va@us.ibm.com>
2024-11-01 14:30:57 -0400

    clean up

--- src/codelens/FeedbackCodelensProvider.ts
+++ src/codelens/FeedbackCodelensProvider.ts
@@ -31,7 +31,7 @@ export function removePromptFeedbackCodeLens() {
   _onDidChangeCodeLenses.fire();
 }
 
-const CodeLensTitles = Object.freeze({
+const FeedbackCodeLensTypes = Object.freeze({
   HELPFUL: "Qiskit Code Assistant: Helpful?",
   POSITIVE: "$(thumbsup)",
   NEGATIVE: "$(thumbsdown)",
@@ -39,24 +39,22 @@ const CodeLensTitles = Object.freeze({
 });
 
 class FeedbackCodeLens extends vscode.CodeLens {
-  cmdTitle: string;
+  type: string;
   modelId: string|undefined;
   promptId: string|undefined;
 
   constructor(
     range: vscode.Range,
-    cmdTitle: string,
+    type: string,
     modelId: string|undefined,
     promptId: string|undefined
   ) {
     super(range);
-    this.cmdTitle = cmdTitle;
+    this.type = type;
     this.modelId = modelId;
     this.promptId = promptId;
   }
 }
-
-let disableYesNo = false;
 
 /**
  * FeedbackCodelensProvider
@@ -73,7 +71,6 @@ export class FeedbackCodelensProvider implements vscode.CodeLensProvider {
     // trigger update of codelens on update of completion items
     inlineCompletionsProvider.onDidUpdateCompletionItems((completions: vscode.InlineCompletionList) => {
       if (completions.items.length) {
-        disableYesNo = false;
         _onDidChangeCodeLenses.fire();
       }
     });
@@ -92,51 +89,38 @@ export class FeedbackCodelensProvider implements vscode.CodeLensProvider {
           codeLensData.position
         );
 
-        if (disableYesNo) {
-          this.codeLenses.push(...[
-            new FeedbackCodeLens(range, CodeLensTitles.HELPFUL, codeLensData.modelId, codeLensData.promptId),
-            new FeedbackCodeLens(range, CodeLensTitles.FEEDBACK, codeLensData.modelId, codeLensData.promptId),
-          ]);
-        } else {
-          this.codeLenses.push(...[
-            new FeedbackCodeLens(range, CodeLensTitles.HELPFUL, codeLensData.modelId, codeLensData.promptId),
-            new FeedbackCodeLens(range, CodeLensTitles.POSITIVE, codeLensData.modelId, codeLensData.promptId),
-            new FeedbackCodeLens(range, CodeLensTitles.NEGATIVE, codeLensData.modelId, codeLensData.promptId),
-            new FeedbackCodeLens(range, CodeLensTitles.FEEDBACK, codeLensData.modelId, codeLensData.promptId),
-          ]);
-        }
+        this.codeLenses.push(...[
+          new FeedbackCodeLens(range, FeedbackCodeLensTypes.HELPFUL, codeLensData.modelId, codeLensData.promptId),
+          new FeedbackCodeLens(range, FeedbackCodeLensTypes.POSITIVE, codeLensData.modelId, codeLensData.promptId),
+          new FeedbackCodeLens(range, FeedbackCodeLensTypes.NEGATIVE, codeLensData.modelId, codeLensData.promptId),
+        ]);
       });
     }
 
     return this.codeLenses;
 	}
 
-  public resolveCodeLens(codeLens: vscode.CodeLens, token: vscode.CancellationToken): vscode.ProviderResult<vscode.CodeLens> {
-    if (codeLens instanceof FeedbackCodeLens) {
-      const commandArgs: (string|boolean|(() => void)|undefined)[] = [codeLens.modelId, codeLens.promptId]
-      function callback(): void {
-        console.log(`callback for ${(codeLens as FeedbackCodeLens).cmdTitle}`)
-        disableYesNo = true;
-        _onDidChangeCodeLenses.fire()
-      }
-      switch(codeLens.cmdTitle) {
-        case CodeLensTitles.HELPFUL:
-          commandArgs.length = 0
-          break;
-        case CodeLensTitles.POSITIVE:
-          commandArgs.push(...[true, callback])
-          break;
-        case CodeLensTitles.NEGATIVE:
-          commandArgs.push(...[true, callback])
-          break;
-        case CodeLensTitles.FEEDBACK:
-          break;
-      }
-      codeLens.command = {
-        title: codeLens.cmdTitle,
-        command: codeLens.cmdTitle == CodeLensTitles.HELPFUL ? "" : provideFeedback.identifier,
-        arguments: commandArgs
-      }
+  public resolveCodeLens(
+    codeLens: FeedbackCodeLens,
+    token: vscode.CancellationToken
+  ): vscode.ProviderResult<vscode.CodeLens> {
+    const commandArgs: (string|boolean|(() => void)|undefined)[] = [codeLens.modelId, codeLens.promptId];
+
+    switch(codeLens.type) {
+      case FeedbackCodeLensTypes.HELPFUL:
+        commandArgs.length = 0
+        break;
+      case FeedbackCodeLensTypes.POSITIVE:
+        commandArgs.push(...[true, removePromptFeedbackCodeLens])
+        break;
+      case FeedbackCodeLensTypes.NEGATIVE:
+        commandArgs.push(...[false, removePromptFeedbackCodeLens])
+        break;
+    }
+    codeLens.command = {
+      title: codeLens.type,
+      command: codeLens.type == FeedbackCodeLensTypes.HELPFUL ? "" : provideFeedback.identifier,
+      arguments: commandArgs
     }
     return codeLens
   }
