965c039365622a5dc6862401a3766bb6fc845cc3
Juan Cruz-Benito <juan.cruz.benito@ibm.com>
2025-10-17 18:04:12 +0200

    Adding code headers

--- src/globals/consts.ts
+++ src/globals/consts.ts
@@ -1,3 +1,17 @@
+/**
+* This code is part of Qiskit.
+*
+* (C) Copyright IBM 2025.
+*
+* This code is licensed under the Apache License, Version 2.0. You may
+* obtain a copy of this license in the LICENSE.txt file in the root directory
+* of this source tree or at http:*www.apache.org/licenses/LICENSE-2.0.
+*
+* Any modifications or derivative works of this code must retain this
+* copyright notice, and modified files need to carry a notice indicating
+* that they have been altered from the originals.
+*/
+
 export const BRAND_NAME = "BAM Code";
 export const STATUS_NAME = "qiskitCodeAssistant";
 export const CHAR_LIMIT = 4_000;
